d3aeb6532e276e15a2c357d96a6e36a9164a3732
Vikas <101702902+NotYourEren@users.noreply.github.com>
2024-03-16 23:03:23 +0530

    16March

--- 1_main.cpp
+++ 1_main.cpp
@@ -5,3 +5,5 @@ int main()  {
     cout << "Hello, World!" << endl; //end1 is used to move to the next line means the the string is printed and move to mouse cursor to the next line
     return 0;
 }
+
+/*<< this is called insertion operator*/
--- 3_input.cpp
+++ 3_input.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main (){
     int x;
     cout << "enter the value of x: ";
-    cin >> x;
+    cin >> x; // >> is called extraction operator
     cout << "The value of x is: " << x << endl;
     return 0;
 }
