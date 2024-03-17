e393207740ca7c89ea54ed23ba82065a25cd9e0f
Vikas <101702902+NotYourEren@users.noreply.github.com>
2024-03-17 10:39:43 +0530

    17March

---
+++ 5_addition.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cout<< "Enter the value of a." <<endl;
+    cin>> a;
+    cout<< "Enter the value of b." <<endl;
+    cint>> b;
+    cout<< "The sum is :" a+b ;
+    return 0;
+}
