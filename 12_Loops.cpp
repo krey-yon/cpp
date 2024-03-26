12287b366609f3326b7b7872130cbf8be736a783
Vikas <101702902+NotYourEren@users.noreply.github.com>
2024-03-26 20:19:17 +0530

    loops

--- 12_Loops.cpp
+++ 12_Loops.cpp
@@ -13,8 +13,39 @@ using namespace std;
 int main() {
     int num = 10;
     int i;
-    for (int i = 0; i < num; i++) {
+    for (int i = 0; i < num; i++) {                 //for loop
         cout << "Value of i: " << i << endl;
     }
+    return 0;
+}
+
+//While Loop in cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i = 10;
+    while (i>0)
+    {
+        cout << "Value of i: " << i << endl;
+        i++;
+    }
+    return 0;
+}
+
+//do-while loop in cpp
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i = 10;
+    do
+    {
+        cout << "Value of i: " << i << endl;
+        i--;
+    } while (i >= 0);
     return 0;
 }
